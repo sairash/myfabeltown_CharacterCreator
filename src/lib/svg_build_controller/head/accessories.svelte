dbeef7bd6d8f1f381a5d241d1f8595f7602a3f26
Sairash Gautam <gautamsairash@gmail.com>
2022-09-10 13:23:35 +0545

    Character Selection Created and working.

--- src/lib/svg_build_controller/head/accessories.svelte
+++ src/lib/svg_build_controller/head/accessories.svelte
@@ -1,1 +1,23 @@
-<div class="w-20 h-20 m-2 bg-blue-500 cursor-pointer border-2 border-black rounded-md"></div>
+<script>
+    import {character_set, character} from '../../../store'
+
+    let data = $character_set.head_part.accessories;
+
+
+    function change_value(value) {
+        let changing_value = $character.head_part
+        changing_value.accessories = value
+		character.update(m => Object.assign({}, m, {head_part: changing_value}));
+	}
+</script>
+
+{#each data as part, i}
+
+    <div class="w-20 h-20 m-2  cursor-pointer border-2 border-black rounded-md text-center" class:bg-red-500="{part.command == $character.head_part.accessories}" on:click="{()=> change_value(part.command)}">
+        <div class="w-full flex justify-center">
+            <img src="/src/assets/character_png/{part.command}.png" class="object-contain w-14 h-14" alt="">
+        </div>
+        {part.name}
+    </div>
+
+{/each}
--- src/store.js
+++ src/store.js
@@ -1,8 +1,35 @@
-import { writable } from "svelte/store";
+import { writable, readable } from "svelte/store";
 
 export const character = writable({
   head_part: {
     accessories: "hair",
+    eyes: {
+      right: "right_1",
+      middle: null,
+      left: "left_1",
+    },
+    mouth: "mouth_1",
+  },
+  body_part: {
+    accessories: "tie",
+    hands: {
+      left: "left_1",
+      right: "right_1",
+    },
+    legs: {
+      left: "left_1",
+      right: "right_1",
+    },
+    body_color: "#ffea31",
+  },
+});
+
+export const character_set = readable({
+  head_part: {
+    accessories: [
+      { name: "null", command: "hair" },
+      { name: "crown", command: "crown" },
+    ],
     eyes: {
       right: "right_1",
       middle: null,
