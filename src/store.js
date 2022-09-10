f14d450e367216163e9ffc5d3b385183f620fc06
Sairash Gautam <gautamsairash@gmail.com>
2022-09-11 00:49:31 +0545

    Many new Head Accessories added

--- src/store.js
+++ src/store.js
@@ -31,13 +31,73 @@ export const character_set = readable({
         name: "null",
         command: "hair",
         image:
-          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/hair.png",
+          "https://github.com/sairash/myfabletown_images/raw/main/png/extra/cross.png",
       },
       {
         name: "crown",
         command: "crown",
         image:
           "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/crown.png",
+      },
+      {
+        name: "Donald",
+        command: "donald_hair",
+        image:
+          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/donald_hair.png",
+      },
+      {
+        name: "Emo Hair",
+        command: "emo_hair",
+        image:
+          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/emo_hair.png",
+      },
+      {
+        name: "Frog Hat",
+        command: "frog_hat",
+        image:
+          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/frog_hat.png",
+      },
+      {
+        name: "Brown Hat",
+        command: "brown_hat",
+        image:
+          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/brown_hat.png",
+      },
+      {
+        name: "Hat",
+        command: "hat",
+        image:
+          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/hat.png",
+      },
+      {
+        name: "Mohawk",
+        command: "mohawk",
+        image:
+          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/mohawk.png",
+      },
+      {
+        name: "Peter Pan",
+        command: "peter_pan",
+        image:
+          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/peter_pan.png",
+      },
+      {
+        name: "Poop",
+        command: "poop",
+        image:
+          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/poop.png",
+      },
+      {
+        name: "Sad Eyes",
+        command: "sad_eyes",
+        image:
+          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/sad_eyes.png",
+      },
+      {
+        name: "Star Hat",
+        command: "star_hat",
+        image:
+          "https://github.com/sairash/myfabletown_images/raw/main/png/head_part/accessories/star_hat.png",
       },
     ],
     eyes: {
